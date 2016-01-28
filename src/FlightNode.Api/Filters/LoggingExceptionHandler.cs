7589ecc40d6269dae409293a439ac0b839d76b08
Stephen Fuqua <stephen.fuqua@trintech.com>
2016-01-27 23:34:44 -0600

    feature/10   arg line endings change. And profile editing.

---
+++ src/FlightNode.Api/Filters/LoggingExceptionHandler.cs
@@ -0,0 +1,33 @@
+﻿using log4net;
+using System.Web.Http.ExceptionHandling;
+
+namespace FlightNode.Common.Api.Filters
+{
+    /// <summary>
+    /// Exception handler of last resort, for when the filters handlers are not used.
+    /// </summary>
+    public class LoggingExceptionHandler : ExceptionHandler, IExceptionHandler
+    {
+        private ILog _logger;
+
+        public ILog Logger
+        {
+            get
+            {
+                return _logger ?? (_logger = LogManager.GetLogger(GetType().FullName));
+            }
+            set
+            {
+                _logger = value;
+            }
+        }
+
+
+        public override void Handle(ExceptionHandlerContext context)
+        {
+            _logger.Error(context.Exception);
+
+            base.Handle(context);
+        }
+    }
+}
