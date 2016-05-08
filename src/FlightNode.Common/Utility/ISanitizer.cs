3c83f6e44a7ca18829ac64243bea62a56e534f43
Stephen Fuqua <stephen@safnet.com>
2016-05-07 22:40:48 -0500

    Tweaking AsNoTracking(), new properties in WorkLog, XSS protection.

---
+++ src/FlightNode.Common/Utility/ISanitizer.cs
@@ -0,0 +1,8 @@
+﻿namespace FlightNode.Common.Utility
+{
+    public interface ISanitizer
+    {
+        string RemoveAllHtml(string input);
+        string RemoveBadHtml(string input);
+    }
+}
---
+++ src/FlightNode.Common/Utility/Sanitizer.cs
@@ -0,0 +1,22 @@
+﻿using Ganss.XSS;
+using System.Collections.Generic;
+
+namespace FlightNode.Common.Utility
+{
+    public class Sanitizer : ISanitizer
+    {
+        
+        public string RemoveAllHtml(string input)
+        {
+            var emptyList = new List<string>();
+            return new HtmlSanitizer(allowedTags: emptyList, allowedAttributes: emptyList, allowedCssProperties: emptyList, allowedSchemes: emptyList)
+                .Sanitize(input);
+        }
+
+        public string RemoveBadHtml(string input)
+        {
+            return new HtmlSanitizer()
+                .Sanitize(input);
+        }
+    }
+}
