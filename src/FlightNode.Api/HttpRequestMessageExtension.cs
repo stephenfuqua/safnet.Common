9b814cf881b2b8d1e11b29edeea38f86c0b383cd
Stephen Fuqua <stephen@safnet.com>
2017-02-14 20:14:53 -0600

    disabled cors - check back on it again

---
+++ src/FlightNode.Api/HttpRequestMessageExtension.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Net.Http;
+using System.Web;
+
+namespace FlightNode.Common.Api
+{
+
+    /// <summary>
+    /// Extension methods for HTTP Request.
+    /// </summary>
+    /// <remarks>
+    /// Useful for trying to debug odd problems with incoming payloads
+    /// </remarks>
+    public static class HttpRequestMessageExtension
+    {
+        public static string ToJsonWithPayload(this HttpRequestMessage request, object payload)
+        {
+
+            return JsonConvert.SerializeObject(new
+            {
+                RequestUri = request.RequestUri,
+                Headers = request.Headers,
+                Body = payload
+            });
+        }
+    }
+
+}
