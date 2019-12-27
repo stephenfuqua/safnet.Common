7ac8fe5a4f84c076110e94361c5d258cfac903de
Stephen Fuqua <stephen@safnet.com>
2019-12-26 23:31:33 -0600

    DbSet modification capabilities

--- TestHelper.AsyncDbSet/src/FakeAsyncDbSet.cs
+++ TestHelper.AsyncDbSet/src/FakeAsyncDbSet.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace safnet.TestHelper.AsyncDbSet
 {
     public class FakeAsyncDbSet<TEntity> : DbSet<TEntity>, IQueryable, IAsyncEnumerable<TEntity>
         where TEntity : class
     {
-        public List<TEntity> List { get; } = new List<TEntity>();
-        
+        public IList<TEntity> List { get; } = new List<TEntity>();
+        public IList<TEntity> Added { get; } = new List<TEntity>();
+        public IList<TEntity> Updated { get; } = new List<TEntity>();
+        public IList<TEntity> Deleted { get; } = new List<TEntity>();
+
         Type IQueryable.ElementType => typeof(TEntity);
 
         Expression IQueryable.Expression => List.AsQueryable().Expression;
@@ -22,9 +26,34 @@ namespace safnet.TestHelper.AsyncDbSet
             return new FakeAsyncEnumerator<TEntity>(List.GetEnumerator());
         }
         
-        public override void AddRange(params TEntity[] entities)
+        public override EntityEntry<TEntity> Add(TEntity entity)
         {
-            List.AddRange(entities);
+            List.Add(entity);
+            Added.Add(entity);
+
+            // Returning null here is only safe because we never want to
+            // do anything with an EntityEntry in this application.
+            return null;
+        }
+
+        public override EntityEntry<TEntity> Update(TEntity entity)
+        {
+            List.Add(entity);
+            Updated.Add(entity);
+
+            // Returning null here is only safe because we never want to
+            // do anything with an EntityEntry in this application.
+            return null;
+        }
+
+        public override EntityEntry<TEntity> Remove(TEntity entity)
+        {
+            List.Add(entity);
+            Deleted.Add(entity);
+
+            // Returning null here is only safe because we never want to
+            // do anything with an EntityEntry in this application.
+            return null;
         }
     }
 }
